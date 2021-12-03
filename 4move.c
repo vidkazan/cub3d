f3d55e938b197106dec499a8b8cfc02f95cc80dc
fcody <vid.kazan@gmail.com>
2021-12-03 22:58:50 +0300

    left right arrows

--- 4move.c
+++ 4move.c
@@ -25,21 +25,46 @@ void rotate(t_data *data, int dir)
 //	pthread_mutex_unlock(data->render_lock);
 }
 
+float get_pos_90deg(float dirX, float dirY , int mode, char proj)
+{
+	float new_dirX = dirX * cos(M_PI_2 * mode) - dirY * sin(M_PI_2 * mode);
+	float new_dirY = dirX * sin(M_PI_2 * mode) + dirY * cos(M_PI_2 * mode);
+	if(proj == 'x')
+		return new_dirX;
+	if(proj == 'y')
+		return new_dirY;
+	return 0;
+}
+
 void move(t_data *data, int dir)
 {
-	if (dir > 0)
+	if (dir == 2)
+	{
+		if(data->map[(int)(data->plr->player_posy)][(int)(data->plr->player_posx + get_pos_90deg(data->plr->dirX,data->plr->dirY, 1, 'x') * data->plr->move_speed)] == '0')
+			data->plr->player_posx += get_pos_90deg(data->plr->dirX,data->plr->dirY, 1, 'x') * data->plr->move_speed;
+		if(data->map[(int)(data->plr->player_posy + get_pos_90deg(data->plr->dirX,data->plr->dirY, 1, 'y') * data->plr->move_speed)][(int)data->plr->player_posx] == '0')
+			data->plr->player_posy += get_pos_90deg(data->plr->dirX,data->plr->dirY, 1, 'y') * data->plr->move_speed;
+	}
+	if (dir == 1)
 	{
 		if(data->map[(int)(data->plr->player_posy)][(int)(data->plr->player_posx + data->plr->dirX * data->plr->move_speed)] == '0')
 			data->plr->player_posx += data->plr->dirX * data->plr->move_speed;
 		if(data->map[(int)(data->plr->player_posy + data->plr->dirY * data->plr->move_speed)][(int)data->plr->player_posx] == '0')
 			data->plr->player_posy += data->plr->dirY * data->plr->move_speed;
 	}
-	if (dir < 0)
+	if (dir == -1)
 	{
 		if(data->map[(int)(data->plr->player_posy)][(int)(data->plr->player_posx - data->plr->dirX * data->plr->move_speed)] == '0')
 			data->plr->player_posx -= data->plr->dirX * data->plr->move_speed;
 		if(data->map[(int)(data->plr->player_posy - data->plr->dirY * data->plr->move_speed)][(int)data->plr->player_posx] == '0')
 			data->plr->player_posy -= data->plr->dirY * data->plr->move_speed;
+	}
+	if (dir == -2)
+	{
+		if(data->map[(int)(data->plr->player_posy)][(int)(data->plr->player_posx - get_pos_90deg(data->plr->dirX,data->plr->dirY, 1, 'x') * data->plr->move_speed)] == '0')
+			data->plr->player_posx -= get_pos_90deg(data->plr->dirX,data->plr->dirY, 1, 'x') * data->plr->move_speed;
+		if(data->map[(int)(data->plr->player_posy - get_pos_90deg(data->plr->dirX,data->plr->dirY, 1, 'y') * data->plr->move_speed)][(int)data->plr->player_posx] == '0')
+			data->plr->player_posy -= get_pos_90deg(data->plr->dirX,data->plr->dirY, 1, 'y') * data->plr->move_speed;
 	}
     render_loop(data);
 }
@@ -49,11 +74,15 @@ int	key_hook(int keycode, t_data *data)
     if (keycode == 13)
         move(data, 1);
     else if (keycode == 0)
-        rotate(data, -1);
+        move(data, -2);
     else if (keycode == 1)
         move(data, -1);
     else if (keycode == 2)
-        rotate(data, 1);
+        move(data, 2);
+    else if (keycode == 123)
+    	rotate(data, -1);
+    else if (keycode == 124)
+    	rotate(data, 1);
     else if (keycode == 53)
         game_close(data);
     return (0);
